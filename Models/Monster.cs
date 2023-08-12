6d57fc66aa445999138c8d403b5d2c9e8c702d8f
Liam James Desmond <liamjdesmond84@gmail.com>
2023-08-12 14:54:10 -0500

    Adding model

---
+++ Models/Monster.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_EFCore.Models
+{
+    public class Monster
+    {
+        [Key]
+        public int MonsterId { get; set; }
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
