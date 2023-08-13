dc17b88268673a6dd9128aedba6ca0119095f592
Liam James Desmond <liamjdesmond84@gmail.com>
2023-08-13 16:46:09 -0500

    Adding DbContext

--- Models/MyContext.cs
+++ Models/MyContext.cs
@@ -4,9 +4,11 @@ namespace DotNet_EFCore.Models
 {
     public class MyContext: DbContext
     {
-
+        // the MyContext class representing a session with our MySQL 
+        // database allowing us to query for or save data
         public MyContext(DbContextOptions options) : base(options) { }
 
+        // the "Monsters" table name will come from the DbSet variable name
         public DbSet<Monster> Monster { get; set; }
 
     }
