1e022cacc3c1806c8d24e99bcdce0bcb33ee221d
Liam James Desmond <liamjdesmond84@gmail.com>
2023-08-13 16:45:48 -0500

    Adding DbContext

--- Models/MyContext.cs
+++ Models/MyContext.cs
@@ -4,5 +4,10 @@ namespace DotNet_EFCore.Models
 {
     public class MyContext: DbContext
     {
+
+        public MyContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Monster> Monster { get; set; }
+
     }
 }
