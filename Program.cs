0dada74efd76e7f7725e435c20783a2fecde0485
Liam James Desmond <liamjdesmond84@gmail.com>
2023-08-13 17:46:56 -0500

    Updating Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 using DotNet_EFCore.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-//builder.Services.AddDbContext(options => options.UseMySql(Configuration["DBInfo:ConnectionString"]));
-//var connectionString = builder.Configuration.GetConnectionString("DBInfo:ConnectionString") ?? throw new InvalidOperationException("Connection string 'DBInfo:ConnectionString' not found.");
-//builder.Services.AddDbContext<MyContext>(options => options.UseMySql(connectionString));
-builder.Services.AddDbContext<MyContext>(options => options.UseMySql("DBInfo:ConnectionString", ServerVersion.AutoDetect("DBInfo:ConnectionString")));
+
+var connectionString = builder.Configuration.GetConnectionString("DBInfo:ConnectionString") ?? throw new InvalidOperationException("Connection string 'DBInfo:ConnectionString' not found.");
+builder.Services.AddDbContext<MyContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+//builder.Services.AddDbContext<MyContext>(options => options.UseMySql("DBInfo:ConnectionString", ServerVersion.AutoDetect("DBInfo:ConnectionString")));
 builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 
 var app = builder.Build();
