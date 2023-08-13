2f0cbc2fe79236c12379dfeb6eaca7b09aee34a2
Liam James Desmond <liamjdesmond84@gmail.com>
2023-08-13 17:37:27 -0500

    Updating Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 //builder.Services.AddDbContext(options => options.UseMySql(Configuration["DBInfo:ConnectionString"]));
-//builder.Services.AddDbContext<MyContext>(options => options.UseMySql(Configuration["DBInfo:ConnectionString"]));
+//var connectionString = builder.Configuration.GetConnectionString("DBInfo:ConnectionString") ?? throw new InvalidOperationException("Connection string 'DBInfo:ConnectionString' not found.");
+//builder.Services.AddDbContext<MyContext>(options => options.UseMySql(connectionString));
 builder.Services.AddDbContext<MyContext>(options => options.UseMySql("DBInfo:ConnectionString", ServerVersion.AutoDetect("DBInfo:ConnectionString")));
 builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 
